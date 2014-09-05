2b4aa96b56f30a05f787788dd2bf13a41e9b34cb
Chris Thompson <chris.thompson@govdelivery.com>
2014-09-05 12:01:19 -0500

    add os_type to cli options

--- lib/rovfer/cli.rb
+++ lib/rovfer/cli.rb
@@ -8,6 +8,9 @@ module Rovfer::CLI
     option :networks, type: :array
     option :system_type, default: 'vmx-09', desc: 'vsphere version/compatibility'
     option :scsi_controller_type, default: 'VirtualSCSI'
+    option :os_type, default: 'Linux', aliases: ['-t']
+    option :os_version, default: "CentOS\ 4/5/6\ (64-bit)", desc: 'default is for an example', aliases: ['-v']
+    option :os_description, default: ""
     def to_vmware(path)
       xml_path = File.expand_path( path )
       bkup     =  xml_path + '.bkup'
@@ -18,8 +21,9 @@ module Rovfer::CLI
       say "wrote backup #{bkup}", :green
       parser = Rovfer::Parser.new xml_path
       parser.networks = options[:networks] if options[:networks]
-      parser.system_type = options.fetch(:system_type)
-      parser.scsi_controller_type= options.fetch(:scsi_controller_type)
+      parser.system_type = options[:system_type]
+      parser.set_os_type( options[:os_type], options[:os_version], options[:os_description] )
+      parser.scsi_controller_type= options[:scsi_controller_type]
       parser.disk_image_config['backing.writeThrough'] = true #not sure what this does, but it was set in an export
       parser.add_special_vmware_config #hot resize ram values and such
       parser.save #overwrites the ovf file
